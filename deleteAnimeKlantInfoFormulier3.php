ad05ca17fa2d8a44a833b8c68fb50b2db5b9381b
Mensje <9014446@leeling.zadkine.nl>
2023-05-26 14:09:48 +0200

    DeleteAnime

---
+++ deleteAnimeKlantInfoFormulier3.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>delete student formulier 3</title>
+</head>
+<body>
+<h1>delete student formulier 3</h1>
+
+<?php
+require "Student.php";
+
+$studentid = $_POST["studentIdVak"];
+$verwijderen = $_POST["verwijderBox"];
+
+if ($verwijderen=="ja")
+{
+    echo "De student is verwijderd <br/>";
+    $student1 =  new AnimeKlantInfo();
+    $student1->deleteAnimeKlantInfo($studentid);
+}
+else
+{
+    echo "De student is niet verwijderd <br/>";
+}
+?>
+<a href="schoolmenu.php">Terug naar het hoofdmenu</a>
+</body>
+</html>
