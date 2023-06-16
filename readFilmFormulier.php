209d0f01133311ab50a8c64a50c42fc4acef7b82
Jasvoetball19 <130988301+Jasvoetball19@users.noreply.github.com>
2023-06-17 00:53:33 +0200

    Update readFilmFormulier.php

--- readFilmFormulier.php
+++ readFilmFormulier.php
@@ -2,35 +2,7 @@
 <html>
     <!--jasper-->
     <head>
-    <style>
-          ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-  }
-  
-  li {
-    float: left;
-  }
-  
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 10px 16px;
-    text-decoration: none;
-  }
-  
-  li a:hover {
-    background-color: #111;
-  }
-</style>
-
-<ul>
-<li><a class="active" href="home.php">Home</a></li>
-</ul>
+   
         <title>read film formulier</title>
         <link rel="stylesheet" href="styler.css">
     </head>
