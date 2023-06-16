16a9b6849c655a7c4214a4bfa9c98663b44a44ac
Jasvoetball19 <130988301+Jasvoetball19@users.noreply.github.com>
2023-06-16 22:44:08 +0200

    Update home.php

--- home.php
+++ home.php
@@ -14,10 +14,10 @@
 
 
         <p>films</p>
-        <a href="createFilmFormulier1.php">    create film</a><br/>
-        <a href="readFilmFormulier.php">       read film </a></br>
+        <a href="createFilmFormulier1.php">    toevoegen film</a><br/>
+        <a href="readFilmFormulier.php">       lezen film </a></br>
         <a href="updateFilmFormulier1.php">    update film </a><br/>
-        <a href="deleteFilmFormulier1.php">    delete film </a><br/>
+        <a href="deleteFilmFormulier1.php">    verwijderen film </a><br/>
         <a href="searchFilmFormulier1.php">    zoeken film </a><br/>
         <br>
         <img src="images/popcorn-1433327_1280.jpg" alt="popcorn" style="width:300px;height:300px;">
