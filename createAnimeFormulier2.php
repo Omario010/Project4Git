d41a2b5f556341c382894189ea666512fa0ec69a
Mensje <9014446@leeling.zadkine.nl>
2023-06-16 12:00:28 +0200

    Create

--- createAnimeFormulier2.php
+++ createAnimeFormulier2.php
@@ -26,3 +26,44 @@ $student1->afdrukkenAnimeKlantInfo();	// drukt objectgegevens af
 <a href="Project4Menu.php">Terug naar het hoofdmenu</a>
 </body>
 </html>
+
+<?php
+
+
+$databaseHost = 'localhost';   //your db host
+$databaseName = 'project4';  //your db name
+$databaseUsername = 'root';    //your db username
+$databasePassword = '';//   db password
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+$id = $_GET['id'];
+
+
+
+$sql="select * from competitors where (id='$id');";//  check id is already copied
+
+$res=mysqli_query($mysqli,$sql);
+
+if (mysqli_num_rows($res) > 0) {
+    // output data of each row
+    $row = mysqli_fetch_assoc($res);
+    if($id==$row['id'])
+    {
+        echo "Already copied"; //error message if already copied
+
+    }
+
+} else{
+
+
+
+    $query=mysqli_query($mysqli,"INSERT INTO animeKlantInfo
+SELECT * FROM  Anime WHERE id =$id");// copy one table to another
+
+    echo "Successfully copied";
+
+
+}
+
+?>
