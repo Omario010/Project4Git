57bb6d85d9ff58ba9fff5c5c59d860a7e43c15c9
Jasvoetball19 <130988301+Jasvoetball19@users.noreply.github.com>
2023-06-17 00:49:32 +0200

    Update createFilmFormulier1.php

--- createFilmFormulier1.php
+++ createFilmFormulier1.php
@@ -6,35 +6,7 @@
 		<link rel="stylesheet" href="styler.css">
 	</head>
 	<body>
-	<style>
-    ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-  }
-  
-  li {
-    float: left;
-  }
-  
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 10px 16px;
-    text-decoration: none;
-  }
-  
-  li a:hover {
-    background-color: #111;
-  }
-  </style>
 
-<ul>
-  <li><a class="active" href="home.php">Home</a></li>
-</ul>
 
 		<h1>toevoegen films formulier 1</h1>
 		<form action="createFilmFormulier2.php" method="post">
