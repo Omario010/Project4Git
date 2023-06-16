0eafe95864a1e10cb78c062f12d79766faf01cae
Jasvoetball19 <130988301+Jasvoetball19@users.noreply.github.com>
2023-06-16 22:34:22 +0200

    Update createFilmFormulier2.php

--- createFilmFormulier2.php
+++ createFilmFormulier2.php
@@ -32,7 +32,7 @@
 <li><a class="active" href="home.php">Home</a></li>
 </ul>
 		<title>create films formulier 2</title>
-		<link rel="stylesheet" href="main.css">
+		<link rel="stylesheet" href="styler.css">
 	</head>
 	<body>
 		<h1>create films formulier 2</h1>
